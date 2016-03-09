16487b2659fc3787ea84dfb140df2e8f3140ed8a
neskoskin <neskoskin@gmail.com>
2016-03-10 00:29:22 +0100

    Config change

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	'port': process.env.PORT || 1337,
-	'database': 'mongodb://localhost/test',
+	//'database': 'mongodb://localhost/test',
+	'database': 'mongodb://eu-1.evennode.com/b919a330caca666ab7cf1a6ab2c44e3e',
 	'secret': 'foohasasecret'
 };
